30e710ec0c0a2f409d24a522ec8a117ce44aad94
Cristian <cristian.cifuentes.guerra@cr.ey.com>
2021-07-19 17:59:25 -0600

    hola1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonService } from './json.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Prueba-Angular';
+  constructor(public json: JsonService){
+    this.json.getJson('../assets/ofertas.json').subscribe((res:any)=>{
+    });
+  }
 }
